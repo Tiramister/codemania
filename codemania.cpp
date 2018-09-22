27dc41c2e7d70a78e8acaf5f4659862a7dfde1df
Tiramister <nishi31416@docomo.ne.jp>
2018-09-22 15:59:27 +0900

    標準入出力(仮)の実装

--- codemania.cpp
+++ codemania.cpp
@@ -1,12 +1,15 @@
 #include <fstream>
 #include <iostream>
+#include <queue>
 #include <string>
 #include <vector>
 using namespace std;
 using ll = long long;
 
 vector<ll> nums;
-vector<char> chars;
+queue<ll> numq;
+vector<string> strs;
+
 
 pair<bool, int> parse(string S) {
     while (S.size() < 9) {
@@ -23,6 +26,50 @@ pair<bool, int> parse(string S) {
     return make_pair(con, res);
 }
 
+void execute(int var) {
+    switch (var) {
+    case 0b10100000: {
+        // 文字列の入力
+        cout << "input str" << endl;
+        string str;
+        cin >> str;
+        strs.push_back(str);
+        break;
+    }
+    case 0b01100000: {
+        // 数値の入力
+        cout << "input num" << endl;
+        ll n;
+        cin >> n;
+        nums.push_back(n);
+        break;
+    }
+    case 0b10010000: {
+        // 文字列の出力
+        cout << "output str" << endl;
+        cout << strs.back();
+        strs.pop_back();
+        break;
+    }
+    case 0b10010001: {
+        // スペースの出力
+        cout << " ";
+        break;
+    }
+    case 0b10010010: {
+        // 改行の出力
+        cout << endl;
+        break;
+    }
+    case 0b01010000: {
+        // 数値の出力
+        cout << "output num" << endl;
+        cout << nums.back();
+        nums.pop_back();
+        break;
+    }
+    }
+}
 
 int main(int argc, char* argv[]) {
     cout << argv[1] << endl;
@@ -49,6 +96,16 @@ int main(int argc, char* argv[]) {
         cout << con << " " << var << endl;
     }
     ifs.close();
+
+    int now = opes.size() - 1;
+    while (now > 0) {
+        bool con;
+        int var;
+        tie(con, var) = opes[now];
+
+        execute(var);
+        --now;
+    }
 
     return 0;
 }
